a1f2acdcb585b956c3f64064a8106b952fa8738f
eyorokumo <eyorokumodicksonemmanuel@gmail.com>
2024-02-02 19:35:38 +0100

    modified

---
+++ fundamental prt1/scripts.js
@@ -0,0 +1,94 @@
+// let js = "amazing";
+// // if (js === "amazing") alert("javascript is FUN!");
+
+// // 40 + 8 + 23 - 10;
+// console.log(40 + 8 + 23 - 10);
+
+// //values and variables name convenation
+// console.log("emmanuel");
+// console.log(23);
+// let firstName = "smart";
+// console.log(firstName);
+
+// // naming convenation
+
+// let jonas_matilda = "JM";
+// let $function = "27";
+
+// let person = "emman";
+
+// let PI = 3.1415;
+
+// let myFirstJob = "programmer";
+// let myCurrentJob = "Teacher";
+// // wrong naming convenation
+// let job1 = "programmer";
+// let job2 = "teacher";
+
+// console.log(myFirstJob);
+
+// ========================DATA TYPES===============
+// ===========PRIMITIVES DATA TYPES ++++++ THEY IN 7 FACES
+// Number:floating point number>used for deciamls and integers.eg /let age = 23; okay
+// String:sequence of characters >used for text e.g/ let firstname = 'emmanuel';
+// Boolean:logical type that can only be true or false >used for taking decisions e.g/let fullAge = 'true';
+// Undefined:value taken by a variable that is not yet defined ('empty value')e.g/let children;
+// Null : Also means 'empty value'
+// Symbol(ES2015):value that is unique and cannot be changed {not useful for now}
+// Bigint(ES2020):Larger integers than the Number type can hold
+
+// Note:Javascript has dynamic typing:we do not have to manually define the data type of the value stored in a variable .Instead , data types are determined automatically.>it's means value has type,NOT variable!
+// let demo some codes
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof "Jonas");
+
+// javascriptIsFun = "YES!";
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+// console.log(typeof null);
+
+//========Let,const and var====>
+let age = "30";
+age = 31;
+
+// const birthday = "1991";
+// birthday = '1990' wrong format
+
+// var job = "programmer";
+// job = "teacher";
+
+// lastName = "smartman";
+// console.log(lastName);
+
+// ====== Basic Operators=====>
+// =====maths operators =>
+const ageSmart = 2037 - 1995;
+const ageFashion = 2037 - 2003;
+console.log(ageSmart, ageFashion);
+
+console.log(ageSmart * 2, ageSmart / 10, 2 ** 3);
+// 2**means 2 to the power of 3 = 2*2*2
+
+const firstName = "Emman";
+const lastName = "Smartmann";
+console.log(firstName + "" + lastName);
+// =====ASSIGNMENT OPerators ==>
+let x = 10 + 5; //15
+x += 10; //x=x+10=25
+x *= 4; //x=x*4=100
+x++; // x=x+1
+x--;
+x--;
+console.log(x);
+
+// comparison operetors
+console.log(ageSmart > ageFashion); //>,<,>=,<=
+console.log;
